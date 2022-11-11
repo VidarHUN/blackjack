29e0cd9b095269720cb5f4640becc35f4ab833e0
Richard Varadi <ricsi19981007@gmail.com>
2022-11-11 20:31:37 +0100

    Define API

--- server.js
+++ server.js
@@ -11,6 +11,73 @@ app.get('/', (req, res) => {
     // res.send(deck.cards);
 });
 
+// Create a new room
+app.post('/newRoom', (req, res) => {
+    // TODO: Create a new room
+    // TODO: Create a new dealer
+    // TODO: Put the user into the room
+    return;
+});
+
+// Join into a room
+// example '/room?user=userID'
+app.get('/room', (req, res) => {
+    // TODO: Find a random not full room
+    // TODO: Put the user into the room
+    return;
+});
+
+// Add chips
+// '/addChips?room=roomID&user=userID&chips=amountOfChips'
+app.post('/addChips', (req, res) => {
+    // TODO: Check if this room exists and the user is in there
+    // TODO: Increase the user's chips
+    return;
+});
+
+// Subsctract chips
+// '/subChips?room=roomID&user=userID&chips=amountOfChips'
+app.post('/addChips', (req, res) => {
+    // TODO: Check if this room exists and the user is in there
+    // TODO: Decrease the user's chips
+    return;
+});
+
+// Hit - request a card
+// '/hit?room=roomID&user=userID'
+app.get('/hit', (req, res) => {
+    // Give a card to the defined user
+    return;
+});
+
+app.get('/double', (req, res) => {
+    return;
+});
+
+app.get('/split', (req, res) => {
+    return;
+});
+
+app.get('/insurance', (req, res) => {
+    return;
+});
+
+app.get('/surrender', (req, res) => {
+    return;
+});
+
+app.get('/bust', (req, res) => {
+    return;
+});
+
+app.get('/push', (req, res) => {
+    return;
+});
+
+app.get('/blackjack', (req, res) => {
+    return;
+});
+
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 });
