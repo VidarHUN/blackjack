338065626b75743faa18289d3eb5b77271a4f6d1
Richard Varadi <ricsi19981007@gmail.com>
2022-11-28 16:13:34 +0100

    Add card images

--- app/src/pages/game.js
+++ app/src/pages/game.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useSearchParams } from 'react-router-dom';
 import axios from 'axios';
 
-
 const Game = () => {
     const [params] = useSearchParams()
     
@@ -32,7 +31,7 @@ const Game = () => {
 
     return (
         <div>
-            <h1>Play blackjack with us! (Game page)</h1>
+            <h1>Game</h1>
             <button onClick={handleHitButton} id="hitButton">Hit</button>
             <button onClick={handleDoubleButton} id="doubleButton">Double</button>
             <button onClick={handleSplitButton} id="splitButton">Split</button>
